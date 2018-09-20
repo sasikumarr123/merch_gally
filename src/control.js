531496f1f16a61eef6cd3114bfd5177916183403
Sasikumar. R <sasikumar.r@impelsys.com>
2018-09-20 20:17:24 +0530

    controlfiles

---
+++ src/control.js
@@ -0,0 +1,22 @@
+var GalaxyUnits = {};
+var GalaxyCurrencies = {};
+
+var romanCurrencies = [i, v, x, l, c, d, m];
+var romanCurrencyVal = {i: 1, v:5, x:10, l:50, c:100, d:500, m:1000}; // L=250 wrongly given as per wiki l=50
+
+//Validate Roman
+
+//Currency to Value (Gallaxy / Roman)
+
+//Handle Galaxy Currency Assign
+
+//Handle Galaxy Credit/Units Assign
+
+//Handle How much
+
+//Handle How many
+
+//Error
+
+//Export
+
