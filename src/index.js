05727a59a22f2a640635d21551a3dde66f7636b1
sasikumarr123 <sasikumar.rajeswaran@gmail.com>
2018-09-22 11:25:03 +0530

    index linked to control

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 var fileSystem = require('fs');
 var lineReading = require('readline');
+var controlConvertion = require('./control.js')
 
 var read = lineReading.createInterface({input: fileSystem.createReadStream('./input.txt')});
 read.on('line', (line) => { 
-    console.log(line.trim())
+    //console.log(line.trim())
+    controlConvertion(line);
 });
