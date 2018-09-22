a3d8455bf26c5570498d0a99fbdb9ec3458d462d
sasikumarr123 <sasikumar.rajeswaran@gmail.com>
2018-09-22 15:14:06 +0530

    how many and how much added

--- src/control.js
+++ src/control.js
@@ -5,9 +5,50 @@ var lineSplit = [];
 var romanCurrencies = ['i', 'v', 'x', 'l', 'c', 'd', 'm'];
 var romanCurrencyVal = {i: 1, v:5, x:10, l:50, c:100, d:500, m:1000}; // L=250 wrongly given as per wiki l=50
 
-//Validate Roman
+//Validate Roman /^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$|^\d+$/
+//https://stackoverflow.com/questions/39200657/how-do-you-match-valid-integers-and-roman-numerals-with-a-regular-expression
+
+var isValidRoman = new RegExp(/^m{0,4}(cm|cd|d?c{0,3})(xc|xl|l?x{0,3})(ix|iv|v?i{0,3})$|^\d+$/)
 
 //Currency to Value (Gallaxy / Roman)
+function convertCurrencytoValue(gCurrency){
+    var cRoman = "";
+    var cRomanArr = [];
+    var finalValue = 0;
+    for (var i = 0; i < gCurrency.length; i++){
+        if(GalaxyCurrencies[gCurrency[i]]){
+            cRoman += GalaxyCurrencies[gCurrency[i]];
+            cRomanArr.push(GalaxyCurrencies[gCurrency[i]]);
+        }else{
+            console.log("Invalid Currency");
+            return -1;
+        }
+    }
+    //console.log(gCurrency+ " : "+ cRoman);
+    if(isValidRoman.test(cRoman)){
+        //console.log(cRomanArr);
+        var romanValueArr = [];
+        cRomanArr.forEach(function (item, index) {
+            romanValueArr.push(romanCurrencyVal[item]);
+            if (romanCurrencyVal[item] < romanCurrencyVal[cRomanArr[index + 1]]) {
+                romanValueArr[index] *= -1;
+            }
+            //console.log(RomanDigits);
+        });
+
+        
+        finalValue = romanValueArr.reduce(function(total, num){
+            return total + num;
+        })
+        
+        //console.log(finalValue);
+        return finalValue;
+    }else{
+        console.log("Currency formed out of rule");
+        return -1;
+    }
+
+}
 
 //Handle Galaxy Currency Assign 
 function galaxyCurrency(data){
@@ -29,11 +70,47 @@ function galaxyUnits(data){
     if(GalaxyCurrencies[creditUnit]){
         console.log(creditUnit + ' is not a unit')
     }
-    console.log(creditUnit);
+    var tValue = convertCurrencytoValue(sliceArray);
+    if(tValue !== -1){
+        tValue = creditValue / tValue;
+        GalaxyUnits[creditUnit] = tValue;
+    }
+    else{
+        console.log('Not a valid currency');
+    }
+    //console.log(GalaxyUnits);
 }
 
 function convertion(data){
+    var findIs = data.indexOf('is');
+    //How Much  
+    if (data.indexOf('much') !== -1){
+        var len = data.length - findIs -2; //(total lenght -1 + startPos -1) 
+        var sliceArray = data.splice(findIs+1, len);
+        //console.log(sliceArray, len)
+        var tValue = convertCurrencytoValue(sliceArray);
+        if(tValue !== -1){
+            console.log(sliceArray.join(' ')+' is ' +tValue);
+        } else {
+            console.log('Not a valid currency');
+        }
+    }
 
+    //How Many
+    if(data.indexOf('many') !== -1){
+        var len = data.length - findIs - 2;
+        var sliceArray = data.splice(findIs+1, len);
+        var creditUnit = sliceArray.pop();
+        var tValue = convertCurrencytoValue(sliceArray);
+
+        if(tValue !== -1){
+            tValue *= GalaxyUnits[creditUnit];
+            console.log(sliceArray.join(' ') + ' '+ creditUnit + ' is ' + tValue + ' credits')
+        } else {
+            console.log('Not a valid currency');
+        } 
+    }
+    
 }
 
 module.exports = function(line){
